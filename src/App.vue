e9c29575700366531611fe79c6ee7b50751c690d
이은혜 <leh941123@naver.com>
2022-10-23 22:13:28 +0900

    일일 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<template>
-  <div class="name">{{ name }}</div> 
+<template> 
+<!-- v-bind: :생략가능 -->
+<!-- v-on: = @ -->
+<!-- v-medel value 바인딩도 해주고, 값이 변경될때 데이터까지 같이 업데이트해주는 역활까지 제공.  -->
+  <!-- <div :class="nameClass">{{ name }}</div>  -->
+  <input 
+  type="text" 
+  v-model="name"
+  >
+  <!-- <input v-bind:type="type" v-bind:value="name"> -->
   <button
-    v-on:click="updateName"
+    @click="onSubmit"
     class="btn btn-primary"
     >
     click
@@ -13,24 +21,39 @@ import { ref } from 'vue';
 export default {
   setup() {
     const name = ref('EunHye');
+    // const type = ref('number');
+    // const nameClass = ref('');
+
     // reactive
     // const name = reactive([]);
-    //배열이나 오브젝트를 사용 시 reactive로 사용해보자.
+    //arrey나 오브젝트를 사용 시 reactive로 사용해보자.
 
     // const greeting = (name) => {
     //   return 'Hello, '+ name;
     // };
     // const greet = greeting(name);
 
-    const updateName = () => {
+    // const updateName = () => {
       // ref .value를 꼭 사용!
-      name.value = 'EunHye Sun';
-      console.log(name);
-    }
+      // name.value = 'Sun';
+      // type.value = 'text';
+      // nameClass.value = 'name';
+      // console.log(name);
+    // }
+    const onSubmit = () => {
+      console.log(name.value);
+    };
+    // const updateName = (e) => {
+    //   name.value = e.target.value;
+    //   // console.log(e.target.value);
+    // }
 
     return {
       name,
-      updateName
+      onSubmit,
+      // updateName,
+      // type,
+      // nameClass
       // greeting,
       // greet 
     }
@@ -41,3 +64,4 @@ export default {
 <style>
 .name {color:green;font-size:20px;font-weight: bold;}
 </style>
+<!-- 태그의 속성값에 v-bind 사용 -->
